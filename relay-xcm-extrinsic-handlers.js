1c987a5eaa71b9a37c3f12c33ff17b4f4937cf0f
serkul <kulyahtin@gmail.com>
2022-07-07 16:07:58 +0200

    started to move generic functions to /common/, added function to calculate sovereign accounts

--- relay-xcm-extrinsic-handlers.js
+++ relay-xcm-extrinsic-handlers.js
@@ -1,11 +1,21 @@
 const { ApiPromise, WsProvider } = require("@polkadot/api");
-const { blake2AsU8a, blake2AsHex } = require("@polkadot/util-crypto");
+const {
+  blake2AsU8a,
+  blake2AsHex,
+  encodeAddress,
+  decodeAddress,
+} = require("@polkadot/util-crypto");
 const { u8aToHex, stringToU8a } = require("@polkadot/util");
+const {
+  intrustionsFromBytesXCMP,
+} = require("./common/instructions-from-bytes-xcmp");
 
 const rpcProvider = "wss://kusama-rpc.polkadot.io/";
 const blockNumber = 12034879; //ump
-
 // const blockNumber = 12034825; //dmp
+
+// const rpcProvider = "wss://rpc.polkadot.io";
+// blockNumber = 1000;
 
 (async () => {
   // Connect to a node (this is ak public one)
@@ -41,93 +51,80 @@ const blockNumber = 12034879; //ump
     xcmpMessageHash: "",
     xcmpInstructions: [],
 
-    fees: "",
+    feesAssit: "",
+    feeLimit: "",
+
     warnings: "",
   };
 
-  await decodeRelayUMP(api, blockNumber, transfer);
-  // decodeRelayDMP(allBlockExtrinsics, apiAt, transfer);
+  // await decodeRelayUMP(api, blockNumber, transfer);
+  // await decodeRelayDMP(api, blockNumber, transfer);
   delete transfer["xcmpInstructions"];
-  console.log(transfer);
+  // console.log(transfer);
 
+  console.log(calcSovereignAddress(api, "2000", "para", 2));
+  console.log(
+    u8aToHex(
+      decodeAddress(
+        "a3d4zPpCqjQLhdrvRZpHHmZcBizveD1PUuboSdnWyRZbhm59p",
+        false,
+        2092
+      )
+    )
+  );
   process.exit();
 })();
 
-async function decodeRelayDMP(transfer) {
+async function decodeRelayDMP(api, blockNumber, transfer) {
   // Get block hash
   const blockHash = await api.rpc.chain.getBlockHash(blockNumber);
   // Get block by hash
   const signedBlock = await api.rpc.chain.getBlock(blockHash);
-
   // Get a decorated api instance at a specific block
   const apiAt = await api.at(signedBlock.block.header.hash);
-
-  const allBlockEvents = await apiAt.query.system.events();
   const allBlockExtrinsics = signedBlock.block.extrinsics;
-  // Get block with upm info
-  const umpInfoblockHash = await api.rpc.chain.getBlockHash(blockNumber - 1);
-  const umpInfoblockHashsignedBlock = await api.rpc.chain.getBlock(
-    umpInfoblockHash
-  );
-
-  allBlockExtrinsics.method.args[0].downwardMessages.forEach((message) => {
-    // Print the Blake2 Hash of the message
-    console.log(
-      `Blake2 hash of message is: ${u8aToHex(blake2AsU8a(message.msg))}\n`
-    );
-
-    // We recover all instructions
-    let instructions = apiAt.reistry.createType("XcmVersionedXcm", message.msg);
-    if (instructions.isV1) {
-      // Print V1 Message
-      console.log(instructions.asV1.toHuman());
-      if (instructions.isDepositAsset) {
-        console.log("Beneficiary Located At");
-        console.log(instructions.toHuman().DepositAsset.beneficiary);
-      }
-    } else if (instructions.isV2) {
-      instructions.asV2.forEach((instruction) => {
-        // Print V2 Message
-        console.log(instruction.toHuman());
-        if (instruction.isWithdrawAsset) {
-          console.log("Withdraw Asset Located At");
-          console.log(instruction.toHuman().WithdrawAsset[0].beneficiary);
-        }
-      });
+  allBlockExtrinsics.forEach((extrinsic) => {
+    if (
+      extrinsic.method.section == "xcmPallet" &&
+      extrinsic.method.method == "limitedReserveTransferAssets"
+    ) {
+      const {
+        dest,
+        beneficiary,
+        assets,
+        fee_asset_item: feeAsset,
+        weight_limit: weightLimit,
+      } = extrinsic.toHuman().method.args; //ts as any
+      transfer.toParachainId = dest.V1.interior.X1.Parachain.toString();
+      transfer.toAddress =
+        beneficiary.V1.interior.X1.AccountKey20.key.toString();
+      transfer.assetId = assets.V0[0].ConcreteFungible.id.toString();
+      transfer.amount = assets.V0[0].ConcreteFungible.amount.toString();
+      transfer.feeAsset = feeAsset.toString();
+      transfer.feeLimit = weightLimit.Limited.toString();
     }
   });
+}
 
-  // .forEach((extrinsic) => {
-  //   if (
-  //     extrinsic.method.section == "xcmPallet" &&
-  //     extrinsic.method.method == "limitedReserveTransferAssets"
-  //   ) {
-  //     console.log("tut");
-  //     extrinsic.method.args[0].backedCandidates.forEach((candidate) => {
-  //       const fromParaId = candidate.candidate.descriptor.paraId.toString();
-  //       // Check upward messages (from parachain to relay chain)
-  //       candidate.candidate.commitments.upwardMessages.forEach((message) => {
-  //         const messageHash = blake2AsHex(Uint8Array.from(message));
-  //         if (messageHash == transfer.xcmpMessageHash) {
-  //           transfer.fromParachainId = fromParaId;
-
-  //           const instructionsHuman = intrustionsFromBytesXCMP(message, apiAt);
-  //           console.log(instructionsHuman);
-  //           if (typeof instructionsHuman == "string") {
-  //             transfer.warnings += instructionsHuman;
-  //           } else {
-  //             console.log("tut");
-
-  //             transfer.xcmpInstructions = instructionsHuman.map((instruction) =>
-  //               JSON.stringify(instruction, undefined, 2)
-  //             );
-  //             parceRelayUMP(instructionsHuman, transfer);
-  //           }
-  //         }
-  //       });
-  //     });
-  //   } //if (paraIngetern.enter)
-  // });
+function parceRelayUMP(instructions, transfer) {
+  instructions.forEach((instruction) => {
+    Object.keys(instruction).forEach((key) => {
+      switch (key) {
+        case "WithdrawAsset":
+          transfer.amount =
+            instruction.WithdrawAsset[0].fun.Fungible.toString();
+          transfer.assetId = JSON.stringify(instruction.WithdrawAsset[0].id);
+          break;
+        case "BuyExecution":
+          transfer.fees = JSON.stringify(instruction.BuyExecution);
+          break;
+        case "DepositAsset":
+          transfer.toAddress =
+            instruction.DepositAsset.beneficiary.interior.X1.AccountId32.id;
+          break;
+      }
+    });
+  });
 }
 
 async function decodeRelayUMP(api, blockNumber, transfer) {
@@ -200,64 +197,14 @@ async function decodeRelayUMP(api, blockNumber, transfer) {
   }
 }
 
-function intrustionsFromBytesXCMP(messageData, apiAt) {
-  // // We recover all instructions
-  let instructions = apiAt.registry.createType("XcmVersionedXcm", messageData); //ts as any
-
-  // choose appropriate xcm version
-  let asVersion = "not found";
-  for (const versionNum of ["0", "1", "2"]) {
-    if (instructions["isV" + versionNum]) {
-      asVersion = "asV" + versionNum;
-    }
-  }
-  if (asVersion === "not found") {
-    return " - xcmp version not found";
-  } else {
-    let instructionsHuman = [];
-    instructions[asVersion].forEach((instruction) => {
-      instructionsHuman.push(instruction.toHuman());
-    });
-    return instructionsHuman;
-  }
+function calcSovereignAddress(api, parachainId, paraOrSibl, prefix) {
+  return encodeAddress(
+    u8aToHex(
+      Uint8Array.from([
+        ...new TextEncoder().encode(paraOrSibl),
+        ...api.createType("ParaId", parachainId).toU8a(),
+      ])
+    ).padEnd(66, "0"),
+    prefix
+  );
 }
-
-function parceRelayUMP(instructions, transfer) {
-  instructions.forEach((instruction) => {
-    Object.keys(instruction).forEach((key) => {
-      switch (key) {
-        case "WithdrawAsset":
-          transfer.amount =
-            instruction.WithdrawAsset[0].fun.Fungible.toString();
-          transfer.assetId = JSON.stringify(instruction.WithdrawAsset[0].id);
-          break;
-        case "BuyExecution":
-          transfer.fees = JSON.stringify(instruction.BuyExecution);
-          break;
-        case "DepositAsset":
-          transfer.toAddress =
-            instruction.DepositAsset.beneficiary.interior.X1.AccountId32.id;
-          break;
-      }
-    });
-  });
-}
-
-// if (instruction.isWithdrawAsset) {
-//   // console.log(instruction.toHuman().WithdrawAsset[0]);
-//   transfer.amount = instruction
-//     .toHuman()
-//     .WithdrawAsset[0].fun.Fungible.toString();
-//   transfer.assetId = JSON.stringify(
-//     instruction.toHuman().WithdrawAsset[0].id
-//   );
-// }
-// if (instruction.isBuyExecution) {
-//   transfer.fees = JSON.stringify(
-//     instruction.toHuman().BuyExecution
-//   );
-// }
-// if (instruction.isDepositAsset) {
-//   transfer.toAddress =
-//     instruction.toHuman().DepositAsset.beneficiary.interior.X1.AccountId32.id;
-// }
